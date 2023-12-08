a59ad6eea844aa6209fabf18b3117fe6c7900f4d
nada123abomostfa <101100406+nada123abomostfa@users.noreply.github.com>
2023-12-08 19:00:45 +0200

    first commit

--- newFile.js
+++
@@ -1,7 +0,0 @@
-import { app } from '.';
-
-app.use('/', (req, res) => {
-    return res.status(200).json({
-        message: 'Hello from Full-CICD-project using AWS, Docker , Jenkins , Github'
-    });
-});
